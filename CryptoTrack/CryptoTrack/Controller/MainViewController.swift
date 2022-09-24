4aa85b197718e9c0ee3f261ed34bcbce20cf8fdc
Furkan Akçakaya <furkanaxcakaya@gmail.com>
2022-09-24 13:01:57 +0300

    Project init

---
+++ CryptoTrack/CryptoTrack/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTrack
+//
+//  Created by Furkan Akçakaya on 24.09.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoTrack/CryptoTrack/Model/CryptoManager.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoManager.swift
+//  CryptoTrack
+//
+//  Created by Furkan Akçakaya on 24.09.2022.
+//
+
+import Foundation
+
+struct CryptoManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
